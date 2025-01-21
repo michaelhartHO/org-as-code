df20a436e3bdb2111110c529410180f4dae9d2ae
michaelhartHO <144438643+michaelhartHO@users.noreply.github.com>
2025-01-21 16:45:51 +0000

    Add integration test for main server

--- sas-data/skills/000-foundation.ts
+++ sas-data/skills/000-foundation.ts
@@ -1,10 +1,22 @@
 // 000-foundation.ts presents the foundational skills
 
-import {RegistrarInterface} from "../../src/registrar.ts";
+import { RegistrarInterface } from "../../src/registrar.ts";
 
 export function register(registrar: RegistrarInterface) {
   registrar
-    .on("1 Jan 2021")
+    .on("3 Dec 2008")
+    .addSkill({
+      tag: "Python 3.0",
+      description: "Python 3.0 development",
+      reference: "https://docs.python.org/3/whatsnew/3.0.html",
+    })
+    .on("Dec 3, 2015")
+    .addSkill({
+      tag: "PHP 7.0",
+      description: "PHP 7.0 development",
+      reference: "https://www.php.net/ChangeLog-7.php#PHP_7_0",
+    })
+    .on("1st Sept 2018")
     .addSkill({
       tag: "java11",
       description: "Java v11 development",
@@ -14,6 +26,7 @@ export function register(registrar: RegistrarInterface) {
     .addSkill({
       tag: "TypeScript 5",
       description: "TypeScript 5 development",
-      reference: "https://devblogs.microsoft.com/typescript/announcing-typescript-5-7/",
+      reference:
+        "https://devblogs.microsoft.com/typescript/announcing-typescript-5-7/",
     });
 }
---
+++ test/integration/main.test.ts
@@ -0,0 +1,54 @@
+// test with logging:
+// $ VERBOSE=true deno test --allow-all test/integration/main.test.ts
+
+import { assertEquals } from "jsr:@std/assert";
+import { assertHasKeys } from "../helpers.ts";
+
+const VERBOSE = Deno.env.get("VERBOSE") === "true";
+
+Deno.test("Server serves data ok", async () => {
+  const PORT = 51234;
+  const process = new Deno.Command(Deno.execPath(), {
+    args: ["run", "--allow-all", "main.ts"],
+    env: { SERVER_PORT: PORT.toString() },
+    stdout: "piped",
+    stderr: "piped",
+  }).spawn();
+
+  // Give the server some time to start
+  await new Promise((resolve) => setTimeout(resolve, 100));
+
+  VERBOSE && console.log("process.pid", process.pid);
+
+  assertEquals(true, true);
+  try {
+    let response = await fetch(`http://localhost:${PORT}/`);
+    type ResponseData = {
+      timestamp: string;
+      data: string | Record<string, unknown>;
+    };
+    let json: ResponseData = await response.json();
+    VERBOSE && console.log("response json:", json);
+    assertEquals(json.data, "Welcome to org-as-code");
+
+    response = await fetch(`http://localhost:${PORT}/skills`);
+    json = await response.json();
+    VERBOSE && console.log("response json:", json);
+    assertEquals(Object.keys(json.data).length >= 4, true);
+
+    response = await fetch(`http://localhost:${PORT}/people`);
+    json = await response.json();
+    VERBOSE && console.log("response json:", json);
+    assertEquals(Object.keys(json.data).length >= 1, true);
+    const person = Object.values(json.data)[0]!;
+    VERBOSE && console.log(person);
+    assertEquals(Object.keys(person).length, 4);
+    assertHasKeys(person, ["tag", "name", "skills", "type"]);
+  } finally {
+    process.stdout.cancel();
+    process.stderr.cancel();
+    process.kill();
+    // Give server time to stop
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+});
