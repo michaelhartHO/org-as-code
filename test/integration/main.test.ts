b2984de6be8fcb1e0d8216267cd724dc74cfbbd4
michaelhartHO <144438643+michaelhartHO@users.noreply.github.com>
2025-01-24 12:17:53 +0000

    Update index page assertions for html

--- test/integration/main.test.ts
+++ test/integration/main.test.ts
@@ -1,12 +1,13 @@
-// test with logging:
+// run test with logging:
 // $ VERBOSE=true deno test --allow-all test/integration/main.test.ts
 
-import { assertEquals } from "jsr:@std/assert";
+import { assertEquals, assertStringIncludes } from "jsr:@std/assert";
 import { assertHasKeys } from "../helpers.ts";
 
 const VERBOSE = Deno.env.get("VERBOSE") === "true";
 
-Deno.test("Server serves data ok", async () => {
+Deno.test("Server serves data ok", async (t) => {
+  // Execute the app that runs the server
   const PORT = 51234;
   const process = new Deno.Command(Deno.execPath(), {
     args: ["run", "--allow-all", "main.ts"],
@@ -20,30 +21,41 @@ Deno.test("Server serves data ok", async () => {
 
   VERBOSE && console.log("process.pid", process.pid);
 
-  assertEquals(true, true);
   try {
-    let response = await fetch(`http://localhost:${PORT}/`);
-    type ResponseData = {
-      timestamp: string;
-      data: string | Record<string, unknown>;
-    };
-    let json: ResponseData = await response.json();
-    VERBOSE && console.log("response json:", json);
-    assertEquals(json.data, "Welcome to org-as-code");
+    await t.step(
+      `Fetch the index page ok`,
+      async () => {
+        const response = await fetch(`http://localhost:${PORT}/`);
+        assertEquals(response.status, 200);
+        const body = await response.text();
+        assertStringIncludes(body, "<!DOCTYPE html>");
+        assertStringIncludes(body, "Org As Code");
+      },
+    );
 
-    response = await fetch(`http://localhost:${PORT}/skills`);
-    json = await response.json();
-    VERBOSE && console.log("response json:", json);
-    assertEquals(Object.keys(json.data).length >= 4, true);
+    await t.step(
+      `Fetch the api/skills json ok`,
+      async () => {
+        const response = await fetch(`http://localhost:${PORT}/api/skills`);
+        const json = await response.json();
+        VERBOSE && console.log("response json:", json);
+        assertEquals(Object.keys(json.data).length >= 4, true);
+      },
+    );
 
-    response = await fetch(`http://localhost:${PORT}/people`);
-    json = await response.json();
-    VERBOSE && console.log("response json:", json);
-    assertEquals(Object.keys(json.data).length >= 1, true);
-    const person = Object.values(json.data)[0]!;
-    VERBOSE && console.log(person);
-    assertEquals(Object.keys(person).length, 5);
-    assertHasKeys(person, ["tag", "name", "skills", "type"]);
+    await t.step(
+      `Fetch the api/people json ok`,
+      async () => {
+        const response = await fetch(`http://localhost:${PORT}/api/people`);
+        const json = await response.json();
+        VERBOSE && console.log("response json:", json);
+        assertEquals(Object.keys(json.data).length >= 1, true);
+        const person = Object.values(json.data)[0]!;
+        VERBOSE && console.log(person);
+        assertEquals(Object.keys(person).length, 5);
+        assertHasKeys(person, ["tag", "name", "skills", "type"]);
+      },
+    );
   } finally {
     process.stdout.cancel();
     process.stderr.cancel();
